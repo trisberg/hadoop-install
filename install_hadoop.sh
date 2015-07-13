2edda6b662a5a4d2bea183d35aee7d4022182d4e
Thomas Risberg <trisberg@pivotal.io>
2015-07-13 00:29:49 -0400

    Move *-site.xml to etc/hadoop directory

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -2,7 +2,7 @@ echo Downloading Hadoop ...
 wget -q http://mirror.nexcess.net/apache/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
 echo Installing Hadoop ...
 tar xzf hadoop-2.6.0.tar.gz
-cp -f /vagrant/*-site.xml hadoop-2.6.0/etc/hadoop/.
+cp -f /vagrant/etc/hadoop/*-site.xml hadoop-2.6.0/etc/hadoop/.
 sed -i "s/\${JAVA_HOME}/\/usr\/lib\/jvm\/java-1.7.0-openjdk.x86_64/g" hadoop-2.6.0/etc/hadoop/hadoop-env.sh
 set JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
 ./hadoop-2.6.0/bin/hdfs namenode -format
