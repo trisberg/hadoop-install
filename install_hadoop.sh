d4673534ef21aac4512858427d3e4652f2a3c032
Thomas Risberg <trisberg@pivotal.io>
2016-04-20 10:02:01 -0400

    Fix JDK 8 path

--- install_hadoop.sh
+++ install_hadoop.sh
@@ -3,7 +3,7 @@ wget -q http://mirror.nexcess.net/apache/hadoop/common/hadoop-2.7.1/hadoop-2.7.1
 echo Installing Hadoop ...
 tar xzf hadoop-2.7.1.tar.gz
 cp -f /vagrant/etc/hadoop/*-site.xml hadoop-2.7.1/etc/hadoop/.
-sed -i "s/\${JAVA_HOME}/\/usr\/lib\/jvm\/java-1.8.0-openjdk/g" hadoop-2.7.1/etc/hadoop/hadoop-env.sh
-set JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+sed -i "s/\${JAVA_HOME}/\/usr\/lib\/jvm\/java-1.8.0-openjdk.x86_64/g" hadoop-2.7.1/etc/hadoop/hadoop-env.sh
+set JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
 ./hadoop-2.7.1/bin/hdfs namenode -format
 
--- stop_hadoop.sh
+++ stop_hadoop.sh
@@ -1,5 +1,5 @@
 echo Stopping Hadoop ...
-set JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+set JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
 set HADOOP_PREFIX=~/hadoop-2.7.1
 set HADOOP_CONF_DIR=$HADOOP_PREFIX/etc/hadoop
 set YARN_CONF_DIR=$HADOOP_CONF_DIR
